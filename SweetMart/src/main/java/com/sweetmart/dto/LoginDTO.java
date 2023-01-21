4043be83d1d090368eb65b3c1dc67e27cf1f9ffe
GandhamRaghavendra <graghavendramutry13@gmail.com>
2023-01-21 20:00:10 +0530

    small changes

--- SweetMart/src/main/java/com/sweetmart/dto/LoginDTO.java
+++ SweetMart/src/main/java/com/sweetmart/dto/LoginDTO.java
@@ -2,48 +2,13 @@ package com.sweetmart.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class LoginDTO {
 	private String mobile;
 	private String password;
 	private String type;
-
-
-//	public LoginDTO(String mobile, String password, String type) {
-//		super();
-//		this.mobile = mobile;
-//		this.password = password;
-//		this.type = type;
-//	}
-
-//	public LoginDTO() {
-//		super();
-//	}
-//	public String getMobile() {
-//		return mobile;
-//	}
-//	public void setMobile(String mobile) {
-//		this.mobile = mobile;
-//	}
-//	public String getPassword() {
-//		return password;
-//	}
-//	public void setPassword(String password) {
-//		this.password = password;
-//	}
-//	public String getType() {
-//		return type;
-//	}
-//	public void setType(String type) {
-//		this.type = type;
-//	}
-	
-	
-	
-
 }
